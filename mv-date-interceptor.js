79f5e4996c4002b7aa282ef0c6bdd868999b0dda
morki <morki@morki.cz>
2015-12-28 15:44:01 +0100

    fix

--- mv-date-interceptor.js
+++ mv-date-interceptor.js
@@ -1,6 +1,6 @@
 /* date interceptor */
 
-angular.module('mv').service('DateInterceptor', function() {
+angular.module('mv', []).service('DateInterceptor', function() {
 
     var toDate = function(d) {
         var ms, match = d.match(/^(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})[+-](\d{2})\:(\d{2})$/);
